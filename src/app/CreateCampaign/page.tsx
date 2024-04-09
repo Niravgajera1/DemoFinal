479c01e87596762921e6ecc5beab3ea8172e8165
niravgajera1 <niravgajera@gmail.com>
2024-04-09 19:07:48 +0530

    .

--- src/app/CreateCampaign/page.tsx
+++ src/app/CreateCampaign/page.tsx
@@ -3,86 +3,67 @@ import React, { useState } from "react";
 import Navbar from "@/app/components/navbar";
 import Footer from "../components/footer";
 import TextField from "@mui/material/TextField";
-import { useFormik } from "formik";
-import { styled } from "@mui/material/styles";
-import Button from "@mui/material/Button";
-import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import dayjs from "dayjs";
-import { campaingshemas } from "./Campaign.Schema";
-
-const VisuallyHiddenInput = styled("input")({
-  clip: "rect(0 0 0 0)",
-  clipPath: "inset(50%)",
-  height: 1,
-  overflow: "hidden",
-  position: "absolute",
-  bottom: 0,
-  left: 0,
-  whiteSpace: "nowrap",
-  width: 1,
-});
-
-const initialValues = {
-  yourname: "",
-  title: "",
-  category: "",
-  story: "",
-  goal: "",
-  enddate: "",
-  image: "",
-};
 
 const createCampaign = () => {
-  // const [data, setData] = useState({
-  //   yourname: "",
-  //   title: "",
-  //   category: "",
-  //   story: "",
-  //   goal: "",
-  //   enddate: "",
-  //   image: "",
-  // });
-  const currentDate = new Date().toISOString().split("T")[0];
-  const { values, errors, handleBlur, handleChange, handleSubmit } = useFormik({
-    initialValues: initialValues,
-    validationSchema: campaingshemas,
-    onSubmit: async (values) => {
-      //e.preventDefault();
-      console.log(values);
-      const formData = new FormData();
-      formData.append("yourname", values.yourname);
-      formData.append("title", values.title);
-      formData.append("category", values.category);
-      formData.append("story", values.story);
-      formData.append("goal", values.goal);
-      formData.append("enddate", values.enddate);
-      formData.append("image", values.image);
-      // const { yourname, title, category, story, goal, enddate, image } = data;
-
-      try {
-        const res = await fetch("http://localhost:3001/campaign/new", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            // Authorization: "Bearer ${token}",
-          },
-          body: formData,
-        });
-        const data = await res.json();
-        alert(data.message);
-        // if (!res.ok) {
-        //   throw new Error("Failed to post");
-        // }
-        console.log(data);
-      } catch (error) {
-        console.log(error);
-      }
-    },
+  const [data, setData] = useState({
+    yourname: "",
+    title: "",
+    category: "",
+    story: "",
+    goal: "",
+    enddate: "",
+    image: "",
   });
+  const currentDate = new Date().toISOString().split("T")[0];
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleDateChange = (date: any) => {
+    setData((prevData) => ({
+      ...prevData,
+      enddate: date.format("YYYY-MM-DD"),
+    }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    const formData = new FormData();
+    formData.append("yourname", data.yourname);
+    formData.append("title", data.title);
+    formData.append("category", data.category);
+    formData.append("story", data.story);
+    formData.append("goal", data.goal);
+    formData.append("enddate", data.enddate);
+    formData.append("image", data.image);
+    e.preventDefault();
+    try {
+      const res = await fetch("http://localhost:3001/campaign/new", {
+        method: "POST",
+        // headers: {
+        //   "Content-Type": "application/json",
+        // },
+        body: formData,
+      });
+      const data = await res.json();
+      if (res.ok) {
+        alert(data.message);
+      }
+      if (!res.ok) {
+        alert(data.message);
+      }
+
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <>
@@ -98,9 +79,9 @@ const createCampaign = () => {
             <div className=" relative flex flex-row gap-5 items-center justify-center mt-4 mb-10">
               <div className="relative h-11 w-full min-w-[200px]">
                 <TextField
-                  value={values.yourname}
+                  required
+                  value={data.yourname}
                   onChange={handleChange}
-                  onBlur={handleBlur}
                   name="yourname"
                   size="small"
                   fullWidth
@@ -109,13 +90,12 @@ const createCampaign = () => {
                   variant="filled"
                   color="info"
                 />
-                <p className="text-red-500">{errors.yourname}</p>
               </div>
               <div className="relative h-11 w-full min-w-[200px]">
                 <TextField
-                  value={values.title}
+                  required
+                  value={data.title}
                   onChange={handleChange}
-                  onBlur={handleBlur}
                   name="title"
                   size="small"
                   fullWidth
@@ -124,13 +104,12 @@ const createCampaign = () => {
                   variant="filled"
                   color="info"
                 />
-                <p className="text-red-500">{errors.title}</p>
               </div>
               <div className="relative h-11 w-full min-w-[200px]">
                 <TextField
-                  value={values.category}
+                  required
+                  value={data.category}
                   onChange={handleChange}
-                  onBlur={handleBlur}
                   name="category"
                   size="small"
                   fullWidth
@@ -139,14 +118,13 @@ const createCampaign = () => {
                   variant="filled"
                   color="info"
                 />
-                <p className="text-red-500">{errors.category}</p>
               </div>
             </div>
             <div className="relative w-full min-w-[200px] font-black mt-4 mb-6">
               <TextField
-                value={values.story}
+                required
+                value={data.story}
                 onChange={handleChange}
-                onBlur={handleBlur}
                 name="story"
                 multiline
                 size="small"
@@ -156,14 +134,13 @@ const createCampaign = () => {
                 variant="filled"
                 color="info"
               />
-              <p className="text-red-500 font-normal">{errors.story}</p>
             </div>
             <div className="relative flex flex-row gap-10 items-center justify-center mt-4 mb-6">
               <div className="relative h-11 w-full min-w-[200px]">
                 <TextField
-                  value={values.goal}
+                  required
+                  value={data.goal}
                   onChange={handleChange}
-                  onBlur={handleBlur}
                   name="goal"
                   size="small"
                   fullWidth
@@ -172,47 +149,27 @@ const createCampaign = () => {
                   variant="filled"
                   color="info"
                 />
-                <p className="text-red-500">{errors.goal}</p>
               </div>
               <div className="relative  w-full min-w-[200px]">
                 <LocalizationProvider dateAdapter={AdapterDayjs}>
                   <DemoContainer components={["DatePicker"]}>
                     <DatePicker
                       name="enddate" // Set the name attribute to match the field name
-                      value={dayjs(values.enddate)}
-                      onChange={(date) =>
-                        handleChange({
-                          target: { name: "enddate", value: date },
-                        })
-                      }
+                      value={dayjs(data.enddate)}
+                      onChange={handleDateChange}
                       label="Select End Date"
                       minDate={dayjs(currentDate)}
                       format="DD-MM-YYYY"
                     />
-                    <p className="text-red-500">{errors.enddate}</p>
                   </DemoContainer>
                 </LocalizationProvider>
               </div>
             </div>
             <div className="relative h-11 w-full min-w-[200px] justify-items mt-2 mb-4">
               <div className="relative w-full min-w-[200px]">
-                {/* <TextField
-                  value={values.image}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  name="image"
-                  type="file"
-                  size="small"
-                  fullWidth
-                  id="outlined-basic"
-                  label=""
-                  variant="filled"
-                  color="info"
-                /> */}
                 <input
-                  value={values.image}
+                  value={data.image}
                   onChange={handleChange}
-                  onBlur={handleBlur}
                   name="image"
                   type="file"
                 ></input>
